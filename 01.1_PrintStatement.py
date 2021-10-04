e0b01a07211ef17398238dddb9cb682b3f8c4496
Chetan <chesar444@gmail.com>
2021-10-04 09:07:23 +0530

    print range on new lines / on same line

--- 01.1_PrintStatement.py
+++ 01.1_PrintStatement.py
@@ -17,13 +17,31 @@ country = 'India'
 print("Hello", country, "..!")      # formatting multiple statements in a single print() function
 
 
-count_seconds = 30
+count_seconds = 3
 for i in reversed(range(count_seconds + 1)):
     if i > 0:
         print(i, end='..', flush=True)
         time.sleep(1)
     else:
         print('Start')
+
+
+
+# array
+a = [1, 2, 3, 4]
+
+# printing a element in new line
+for i in range(4):
+    print(a[i]),
+
+# printing a element in Same line
+for i in range(4):
+    print(a[i], end=" "),
+
+array = [1, 2, 3, 4, 5, 6]
+
+# using * symbol prints the list,  elements in a single line
+print(*array)
 
 # declare a dummy file
 dummy_file = io.StringIO()
@@ -33,3 +51,4 @@ print('Hello India , This is to write string into File and read it back...!!', f
 
 # get the value from dummy file
 dummy_file.getvalue()
+
